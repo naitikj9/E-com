053f611b26521bd01775765b99c8bc55280dd72b
naitikj9 <naitikjain921@gmail.com>
2024-06-19 11:49:12 +0530

    add functionality to cart

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -2,7 +2,7 @@ import './ProductCard.css';
 import AddToCart from '../AddToCart/AddToCart';
 import { useRef, useState } from 'react';
 import logo1 from '../../assests/logo1.png';
-function ProductCard({ title, price }) {
+function ProductCard({ product, cart, increaseQuantity, decreaseQuantity }) {
   var a = 10;
   a = a + 1;
   let pRef = useRef(0);
@@ -10,33 +10,27 @@ function ProductCard({ title, price }) {
   let oRef = useRef(0);
   let [inputV, setInputV] = useState('Class');
 
-  // setInput(value);
   function printTitle() {
-    // console.log("printTitle");
-    // console.log(pRef.current.innerText);
     if (pRef.current.style.display === "none") {
       pRef.current.style.display = "block";
     } else {
       pRef.current.style.display = "none";
     }
   }
-  // console.log("Rerendered", inputV);
+  
   function displayOutput(e) {
-    // console.log(iRef.current);
-    // console.log(oRef.current);
-    // oRef.current.innerText = `Output here: ${ iRef.current.value }`;
-    // oRef.current.innerText = oRef.current.innerText + iRef.current.value;
+  
     setInputV(e.target.value);
   }
 
   return (
     <div className="product-card">
-      <p onClick={printTitle}> {title}</p>
-      <p ref={pRef}> {price.value}</p>
+      <p onClick={printTitle}> {product.title}</p>
+      <p ref={pRef}> {product.price.value}</p>
       <img src={logo1} />
       <input type="text" onChange={displayOutput} ref={iRef} value={inputV} />
       <p ref={oRef}>Over here the output would arrive - {inputV}</p>
-      <AddToCart />
+      <AddToCart product={product}  cart={cart} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity}/>
     </div>
     )
   }
